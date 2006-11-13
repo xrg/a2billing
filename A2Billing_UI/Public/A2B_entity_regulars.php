0bb9a1c65232c71f4ee4d08ac8994abc581fe24a
Panagiotis Christeas <panos@xorhgos2.pefnos>
2006-11-13 18:46:41 +0200

    disp() function

--- A2Billing_UI/Public/A2B_entity_regulars.php
+++ A2Billing_UI/Public/A2B_entity_regulars.php
@@ -16,61 +16,80 @@ if (!isset($popup_select)) $popup_select = '';
 class FillBoothForm{
 	var $list_booths;
 	var $pref_booth=-1;
+	var $txt_fill ;
+	var $txt_empty;
 	
 	function init(&$DBHandle){
 		global $list_booths;
-		$itb = new Table("cc_booth", "id, name");
+		global $txt_fill;
+		global $txt_empty;
+		global $txt_change;
+		$itb = new Table("cc_booth", "id, name, agentid");
 		//$itb->debug_st=1;
-		$FG_TABLE_CLAUSE = "agentid = ". $DBHandle->Quote($_SESSION["agent_id"]) ." AND cur_card_id IS NULL";
+		$FG_TABLE_CLAUSE = 'def_card_id IS NOT NULL';
+		//"agentid = ". $DBHandle->Quote($_SESSION["agent_id"]) ." AND cur_card_id IS NULL";
 			// A booth without default card couldn't ever become active!
-		$FG_TABLE_CLAUSE .= "AND def_card_id IS NOT NULL";
 		$ltb = $itb -> Get_list ($DBHandle, $FG_TABLE_CLAUSE);
 		$list_booths=$ltb;
+		$txt_fill = _("Fill!");
+		$txt_empty = _("Empty!");
 		
 	}
 
 	function disp($query_row, $qc){
 		global $list_booths;
+		global $txt_fill;
+		global $txt_empty;
 		$ress = '';
 		$opts = '';
 		 // name the fields (for clarity), see FG_var_card's FG_COL_QUERY
 		$f_id = $query_row[0];
-		$f_def = $query_row[4];
-		$f_now_id = $query_row[5];
-		$f_now_name = $query_row[6];
-		$f_def_id = $query_row[7];
-		$f_def_name = $query_row[8];
-		if ($f_now_id != null)
-			$ress .= "<span>" . htmlspecialchars($f_now_name) ."</span>";
-		else {
-			$ress .= _("Nowhere");
+		$f_agentid = $query_row[11];
+		$f_def = $query_row[5];
+		$f_now_id = $query_row[7];
+		$f_now_name = $query_row[8];
+		$f_def_id = $query_row[9];
+		$f_def_name = $query_row[10];
+		
+		if ($f_now_id != null){
+			$f_sp_name = htmlspecialchars($f_now_name);
+			$ress .= <<<EOS
+		<form class="EmptyBooth" action="${_SERVER['PHP_SELF']}" method="GET" >
+		<label>$f_sp_name </label>
+		<input type="hidden" name="action" value="emptyb" />
+		<input type="hidden" name="booth" value="$f_now_id">
+		<button type="submit">$txt_empty</button>
+		</form>
+EOS;
+		} else {
+			$ress .= "<span>" . _("Nowhere") ;
+			if (isset($f_def_id))
+				$ress .= _(", default for ") . htmlspecialchars($f_def_name);
+			$ress.= "</span>";
+			//$ress .= "Default: " . $f_def_id . " " ;
 			//echo gettype($f_def). $f_def;
 			if ($list_booths)
-			foreach($list_booths as $lb){
-				$opts .= '<option value="' . $lb[0] .'"';
-				if ($lb[0]==$pref_booth)
-					$opts .= ' selected';
-				$opts.= '>' . htmlspecialchars($lb[1]);
-				$opts.="</option>\n";
-			}
-			
-			if ($list_booths)
-				$button_dis= '';
-			else
-				$button_dis='disabled';
-			$ress = <<<EOS
+				foreach($list_booths as $lb)
+				if( (!isset($f_def_id)) || ($lb[0] == $f_def_id)){
+					$opts .= '<option value="' . $lb[0] .'"';
+					$opts.= '>' . htmlspecialchars($lb[1]);
+					$opts.="</option>\n";
+				}
+				
+			$ress .= <<<EOS
 		<form class="FillBooth" action="${_SERVER['PHP_SELF']}" method="GET" >
 		<input type="hidden" name="action" value="fillb" />
 		<input type="hidden" name="cardid" value="$f_id" />
 		<select name="booth">
 		$opts
 		</select>
-		<button type="submit" $button_dis> Fill! </button>
+		<button type="submit">$txt_fill</button>
 	</form>
 EOS;
 		}
 		return $ress;
 	}
+
 };
 
 require("PP_header.php");
@@ -95,10 +114,9 @@ require ("./form_data/FG_var_regulars.inc");
 $HD_Form -> init();
 $fb_form->init($HD_Form->DBHandle);
 
-if ($id!="" || !is_null($id)){	
+if ($id!="" || !is_null($id)){
 	$HD_Form -> FG_EDITION_CLAUSE = str_replace("%id", "$id", $HD_Form -> FG_EDITION_CLAUSE);	
 }
-
 
 
 // Fill booth action must be carried out before this, because this queries for the empty ones.
