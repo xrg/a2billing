b16d8fbd3021bfcf2cb9c78e440fdd5ef33625b1
Panagiotis Christeas <panos@xorhgos2.pefnos>
2007-01-02 13:42:01 +0200

    Usage instructions

---
+++ A2BAgent_UI/usage.php
@@ -0,0 +1,43 @@
+<?php
+include ("lib/defines.php");
+include ("lib/module.access.php");
+
+if (! has_rights (ACX_ACCESS)){
+      Header ("HTTP/1.0 401 Unauthorized");
+      Header ("Location: PP_error.php?c=accessdenied");
+      die();
+}
+include ("PP_header.php"); 
+
+/* It would be silly to pass this entire file through gettext().
+   Instead, we could have the usage files under lib/locale/<LANG>/..
+   */
+if (getenv('LANG')!= 'en_US')
+	$res = include "lib/locales/".getenv('LANG')."/usage.php";
+else
+	$res = false;
+	
+if ($res == false){
+	// Copy text below and translate!
+?>
+
+
+<h1>Usage Instructions</h1>
+Dial the number from the VoIP phones!
+
+<h2>Customers </h2>
+Customers can make use of our service. Each customer must be assigned to a Customer ID
+(or "card") so that he/she can make a phone call and be billed.<br>
+At the callshop, there can be two kinds of customers:<br>
+<ul>
+<li><b>Regulars (Members):</b><ht> These are persons who are permanently assigned a card. 
+They have an account with us.</li>
+<li><b>One-timers:</b><ht> These are persons who only come once to use our service. Their
+balance will be settled before they leave. Hopefully, they will come again!</li>
+</ul>
+
+<?php
+} //end if & copy
+
+include ("PP_footer.php"); 
+?>
