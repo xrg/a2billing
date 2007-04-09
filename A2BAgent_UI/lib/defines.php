0a181baec823f9d4565c70c8f4594475c73fad68
Panos Christeas <p_christeas@yahoo.com>
2007-04-09 12:09:49 +0300

    Allow postgres unix socket (no hostname)

--- A2BAgent_UI/lib/defines.php
+++ A2BAgent_UI/lib/defines.php
@@ -116,7 +116,10 @@ require("languageSettings.php");
 	function DbConnect($db= NULL)
 	{
 		if (DB_TYPE == "postgres"){
-			$datasource = 'pgsql://'.USER.':'.PASS.'@'.HOST.'/'.DBNAME;
+			if (HOST == '')
+				$datasource = 'pgsql://dbname='.DBNAME.' user='.USER ;
+			else
+				$datasource = 'pgsql://'.USER.':'.PASS.'@'.HOST.'/'.DBNAME;
 		}else{
 			$datasource = 'mysql://'.USER.':'.PASS.'@'.HOST.'/'.DBNAME;
 		}
