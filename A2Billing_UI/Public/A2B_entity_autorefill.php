cb9184dde4070072479710395235681fbcd4ce3a
areski <areski>
2007-05-17 12:36:54 +0000

    Access rights correction

--- A2Billing_UI/Public/A2B_entity_autorefill.php
+++ A2Billing_UI/Public/A2B_entity_autorefill.php
@@ -6,7 +6,7 @@ include ("./form_data/FG_var_autorefill.inc");
 include ("../lib/smarty.php");
 
 
-if (! has_rights (ACX_RATECARD)){ 
+if (! has_rights (ACX_CRONT_SERVICE)){ 
 	Header ("HTTP/1.0 401 Unauthorized");
 	Header ("Location: PP_error.php?c=accessdenied");	   
 	die();	   
--- A2Billing_UI/Public/A2B_entity_service.php
+++ A2Billing_UI/Public/A2B_entity_service.php
@@ -5,7 +5,7 @@ include ("../lib/Form/Class.FormHandler.inc.php");
 include ("./form_data/FG_var_service.inc");
 include ("../lib/smarty.php");
 
-if (! has_rights (ACX_RATECARD)){ 
+if (! has_rights (ACX_CRONT_SERVICE)){ 
 	   Header ("HTTP/1.0 401 Unauthorized");
 	   Header ("Location: PP_error.php?c=accessdenied");	   
 	   die();	   
