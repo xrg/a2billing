6c293ff28cbea3fc332728faa0a1ad837cf68884
Panos Christeas <p_christeas@yahoo.com>
2007-08-31 11:57:36 +0300

    Function format_currency2 : more precision, suitable for rates

--- DataBase/psql/a2billing-pgsql-callshop.2.sql
+++ DataBase/psql/a2billing-pgsql-callshop.2.sql
@@ -43,6 +43,20 @@ CREATE OR REPLACE FUNCTION format_currency(money_sum NUMERIC, from_cur CHAR(3), 
 	$$
 	LANGUAGE SQL STABLE STRICT;
 	
+CREATE OR REPLACE FUNCTION format_currency2(money_sum NUMERIC, from_cur CHAR(3), to_cur CHAR(3)) RETURNS text
+	AS $$
+	SELECT CASE WHEN sign_pre THEN 
+			csign || ' ' || to_char( ($1 * from_rate) / to_rate, cformat2)
+		ELSE
+			to_char( ($1 * from_rate) / to_rate, cformat2) || ' ' || csign
+		END
+	FROM (SELECT DISTINCT ON (b.currency) a.value AS from_rate,  b.value AS to_rate, b.cformat2, 
+			COALESCE(b.csign,b.currency) AS csign , b.sign_pre 
+		FROM cc_currencies AS a, cc_currencies AS b
+		WHERE a.currency = $2 AND b.currency = $3 AND a.basecurrency = b.basecurrency ) AS foo
+		;
+$$ LANGUAGE SQL STABLE STRICT;
+
 
 CREATE OR REPLACE FUNCTION format_currency(money_sum DOUBLE PRECISION, from_cur CHAR(3), to_cur CHAR(3)) RETURNS text
 	AS $$
