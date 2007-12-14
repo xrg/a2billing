c38f5adbc4b119d5705c11debbcd24aaa145c279
Panos Christeas <p_christeas@yahoo.com>
2007-12-14 11:54:07 +0200

    Rate engine tests

--- DataBase/psql/test.rateengine.sql
+++ DataBase/psql/test.rateengine.sql
@@ -34,7 +34,8 @@
 -- SELECT * from sell_calc_rev_part( 0.00, 0.09, 30, 60, 10);
 
 	-- Outer query: Find matching trunk and buy rates for selling rate found in inner query
-EXPLAIN ANALYZE	SELECT DISTINCT ON (cc_tariffplan.id) allsellrates.*, '12345' AS dialstring, 1 AS tgid,
+EXPLAIN ANALYZE	 SELECT * FROM (
+	SELECT DISTINCT ON (cc_tariffplan.id) allsellrates.*, '12345' AS dialstring, 109 AS tgid,
 		  cc_buyrate.id AS brid, cc_buy_prefix.dialprefix AS prefix,
 		  cc_trunk.id AS trunkid, cc_trunk.trunkcode, cc_trunk.trunkprefix, cc_trunk.providertech,
 		  cc_trunk.providerip, cc_trunk.addparameter AS trunkparm, cc_trunk.provider, '1'::integer AS trunkfree /*-*/,
@@ -45,10 +46,10 @@ EXPLAIN ANALYZE	SELECT DISTINCT ON (cc_tariffplan.id) allsellrates.*, '12345' AS
 			cc_sellrate.id AS srid, cc_sellrate.destination,
 			sell_calc_rev(cc_sellrate.*,10.0) AS tmout
 			FROM cc_sellrate, cc_sell_prefix , cc_retailplan, cc_tariffgroup_plan
-			WHERE cc_sell_prefix.dialprefix = ANY(dial_exp_prefix('1234'))
+			WHERE cc_sell_prefix.dialprefix = ANY(dial_exp_prefix('93099753'))
 				AND cc_sellrate.id = cc_sell_prefix.srid
 				AND cc_retailplan.id = cc_sellrate.idrp
-				AND cc_tariffgroup_plan.tgid = 1
+				AND cc_tariffgroup_plan.tgid = 109
 				AND cc_tariffgroup_plan.rtid = cc_retailplan.id
 				AND ( current_timestamp BETWEEN cc_retailplan.start_date AND cc_retailplan.stop_date)
 			ORDER BY cc_retailplan.id, length(cc_sell_prefix.dialprefix) DESC
@@ -58,10 +59,10 @@ EXPLAIN ANALYZE	SELECT DISTINCT ON (cc_tariffplan.id) allsellrates.*, '12345' AS
 			AND cc_rtplan_buy.tpid = cc_tariffplan.id 
 			AND cc_buyrate.idtp = cc_tariffplan.id
 			AND cc_buyrate.id = cc_buy_prefix.brid
-			AND cc_buy_prefix.dialprefix = ANY(dial_exp_prefix('1234'))
+			AND cc_buy_prefix.dialprefix = ANY(dial_exp_prefix('93099753'))
 -- 			AND ( now() BETWEEN cc_tariffplan.start_date AND cc_tariffplan.stop_date)
 			AND cc_tariffplan.trunk = cc_trunk.id
 		ORDER BY cc_tariffplan.id, length(cc_buy_prefix.dialprefix) DESC
-		;
+		) AS outerq ORDER BY tmout DESC;
 
 -- eof
