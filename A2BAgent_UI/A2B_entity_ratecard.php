bc547def92c481110ee3bef40d7ae26af8456973
Panos Christeas <p_christeas@yahoo.com>
2008-01-11 13:23:32 +0200

    Allow agent to browse (their) rates.

--- A2BAgent_UI/A2B_entity_ratecard.php
+++ A2BAgent_UI/A2B_entity_ratecard.php
@@ -1,72 +1,35 @@
 <?php
-include ("./lib/defines.php");
-include ("./lib/module.access.php");
-include ("./lib/Form/Class.FormHandler.inc.php");
+require_once ("./lib/defines.php");
+require_once ("./lib/module.access.php");
+require_once (DIR_COMMON."Form.inc.php");
+require_once (DIR_COMMON."Class.HelpElem.inc.php");
+require_once (DIR_COMMON."Form/Class.ClauseField.inc.php");
+require_once (DIR_COMMON."Form/Class.TimeField.inc.php");
+require_once (DIR_COMMON."Form/Class.SqlRefField.inc.php");
 
+$menu_section='menu_info';
 
-if (! has_rights (ACX_ACCESS)){
-	   Header ("HTTP/1.0 401 Unauthorized");
-	   Header ("Location: PP_error.php?c=accessdenied");
-	   die();
-}
-/***********************************************************************************/
+HelpElem::DoHelp(_("These are the selling rates to each destination"));
 
-include ("FG_var_ratecard.inc");
+$HD_Form= new FormHandler('cc_agent_current_rates_v',_("Rates"),_("Rate"));
+$HD_Form->checkRights(ACX_ACCESS);
+$HD_Form->init(null,false);
+$HD_Form->views['list']=new ListView();
+//$HD_Form->views['details'] = new DetailsView();
 
-$HD_Form -> init();
+$PAGE_ELEMS[] = &$HD_Form;
 
+//$HD_Form->model[] = new PKeyField(_("ID"),'id');
+$HD_Form->model[] = new ClauseField('agentid',$_SESSION['agent_id']);
 
-if ($id!="" || !is_null($id)){
-	$HD_Form -> FG_EDITION_CLAUSE = str_replace("%id", "$id", $HD_Form -> FG_EDITION_CLAUSE);
-}
+$HD_Form->model[] = new TextField(_("Destination"), "destination");
 
-if (!isset($form_action))  $form_action="list"; //ask-add
-if (!isset($action)) $action = $form_action;
+$HD_Form->model[] = new MoneyField(_("Rate"),'rateinitial');
+$HD_Form->model[] = new MoneyField(_("Charge"),'charge_once');
+$HD_Form->model[] = new SecondsField(_("Block"),'billingblock');
+$HD_Form->model[] = new TextField(_("Tariff"), "tg_name");
+end($HD_Form->model)->fieldexpr = "gettext(tg_name,'".getenv('LANG') ."')";
 
-
-if ( ($form_action == "list") &&  ($HD_Form->FG_FILTER_SEARCH_FORM) && ($_POST['posted_search'] == 1 )){
-	$HD_Form->FG_TABLE_CLAUSE = "tp_id='$mytariff_id'";
-}
-
-$list = $HD_Form -> perform_action($form_action);
-
-
-// #### HEADER SECTION
-include("PP_header.php");
-
-// #### HELP SECTION
-if ($form_action == 'list')
-{
-    echo '<br><br>'.$CC_help_ratecard;
-}
-// The second clause below should effectively protect against injecting a 3rd party ratecard
-// into the url (and thus reading 'hidden' rates)
-
-$HD_Form -> FG_TABLE_CLAUSE = "cc_tariffplan.id = cc_tariffgroup_plan.idtariffplan AND cc_tariffgroup_plan.idtariffgroup = '".$_SESSION["tariff"]."'";
-
-if ($form_action == "list" ) $HD_Form -> create_select_form_client($HD_Form -> FG_TABLE_CLAUSE);
-
-// $HD_Form -> FG_TABLE_CLAUSE .= " cc_tariffgroup_plan.idtariffplan=cc_ratecard.idtariffplan  AND cc_tariffgroup_plan.idtariffgroup = '".$_SESSION["tariff"]."'";
-
-// 'AND cc_ratecard.idtariffplan='".$_SESSION["mytariff_id"]."'
-
- // #### TOP SECTION PAGE
-$HD_Form -> create_toppage ($form_action);
-
-
-// #### CREATE FORM OR LIST
-//$HD_Form -> CV_TOPVIEWER = "menu";
-if (strlen($_GET["menu"])>0) $_SESSION["menu"] = $_GET["menu"];
-
-$HD_Form -> create_form ($form_action, $list, $id=null) ;
-
-// #### CREATE SEARCH FORM
-if ($form_action == "list"){
-	$HD_Form -> create_search_form();
-}
-
-
-// #### FOOTER SECTION
-include("PP_footer.php");
+require("PP_page.inc.php");
 
 ?>
