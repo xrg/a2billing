1ed8cbff590173d9b410b9955c3266ec9f000c7c
Panos Christeas <p_christeas@yahoo.com>
2008-01-13 19:54:16 +0200

    Consolidate Logger, drop Class.Table.

--- common/lib/Class.Logger.inc.php
+++ common/lib/Class.Logger.inc.php
@@ -1,16 +1,7 @@
 <?php
 require_once("Class.A2Billing.inc.php");
-require_once("Class.Table.php");
-/*
-     * File: Class.Logger.php
-     *
-     * Description: Define an Logger class that can be used to log 
-     *              different events happening in the application.
-     *
-*/	 
-
-// *-* Major rewrite needed!
-
+/** Logger can be used to log different events happening in the application.
+*/ 
 class Logger
 {
 	var $do_debug = 0;
@@ -25,77 +16,58 @@ class Logger
 	function insertLog_Add($userID, $logLevel, $actionPerformed, $description, $tableName, $ipAddress, $pageName, $param_add_fields, $param_add_value)
 	{
 		$DB_Handle = A2Billing::DBHandle();
-		$table_log = new Table();
 		$pageName = basename($pageName);
 		$pageName    = array_shift(explode('?', $pageName));
-		$description = str_replace("'", "", $description);
-		$str_submitted_fields = explode(',', $param_add_fields);
-		$str_submitted_values = explode(',', $param_add_value);
-		$num_records = count($str_submitted_fields);
+		$sfields = explode(',', $param_add_fields);
+		$svalues = explode(',', $param_add_value);
+		$num_records = count($sfields);
+		$spairs= array();
 		for($num = 0; $num < $num_records; $num++)
-		{
-			$str_name_value_pair .= $str_submitted_fields[$num]." = ".str_replace("'",'',$str_submitted_values[$num]);
-			if($num != $num_records -1)
-			{
-				$str_name_value_pair .= "|";
-			}
-		}
-		$QUERY = "INSERT INTO cc_system_log (iduser, loglevel, action, description, tablename, pagename, ipaddress, data) ";
-		$QUERY .= " VALUES('".$userID."','".$logLevel."','".$actionPerformed."','".$description."','".$tableName."','".$pageName."','".$ipAddress."','".$str_name_value_pair."')";
-		if ($this -> do_debug) echo $QUERY;
-
-		$table_log -> SQLExec($DB_Handle, $QUERY);
+			$spairs[] = $sfields[$num]."= ". $svalues[$num];
+		$sdata = implode('|',$spairs);
+		
+		$res= $DB_Handle->Execute ("INSERT INTO cc_system_log (iduser, loglevel, action, description, tablename, pagename, ipaddress, data) ".
+				" VALUES(?,?,?,?,?,?,?,?);",
+				array($userID, $logLevel, $actionPerformed, $description, $tableName, $pageName, $ipAddress, $sdata));
+		
+		if (!$res) @syslog(LOG_WARNING,"Cannot log: ". $DB_Handle->ErrorMsg());
 	}
 	
 	function insertLog_Update($userID, $logLevel, $actionPerformed, $description, $tableName, $ipAddress, $pageName, $param_update)
 	{
 		$DB_Handle = A2Billing::DBHandle();
-		$table_log = new Table();
 		$pageName = basename($pageName);
-		$pageName    = array_shift(explode('?', $pageName));
-		$description = str_replace("'", "", $description);
-		$str_submitted_fields = explode(',', $param_update);
-		$num_records = count($str_submitted_fields);
-		for($num = 0; $num < $num_records; $num++)
-		{
-			$str_name_value_pair .= str_replace("'","",$str_submitted_fields[$num]);
-			if($num != $num_records -1)
-			{
-				$str_name_value_pair .= "|";
-			}
-		}
-		$QUERY = "INSERT INTO cc_system_log (iduser, loglevel, action, description, tablename, pagename, ipaddress, data) ";
-		$QUERY .= " VALUES('".$userID."','".$logLevel."','".$actionPerformed."','".$description."','".$tableName."','".$pageName."','".$ipAddress."','".$str_name_value_pair."')";
+		$pageName = array_shift(explode('?', $pageName));
 		
-		if ($this -> do_debug) echo $QUERY;
-
-		$table_log -> SQLExec($DB_Handle, $QUERY);
+		$res= $DB_Handle->Execute ("INSERT INTO cc_system_log (iduser, loglevel, action, description, tablename, pagename, ipaddress, data) ".
+				" VALUES(?,?,?,?,?,?,?,?);",
+				array($userID, $logLevel, $actionPerformed, $description, $tableName, $pageName, $ipAddress, $param_update));
+		
+		if (!$res) @syslog(LOG_WARNING,"Cannot log: ". $DB_Handle->ErrorMsg());
 	}	
 	function insertLog($userID, $logLevel, $actionPerformed, $description, $tableName, $ipAddress, $pageName, $data='')
 	{
 		$DB_Handle = A2Billing::DBHandle();
-		$table_log = new Table();
 		$pageName = basename($pageName);
 		$pageArray = explode('?', $pageName);
 		$pageName = array_shift($pageArray);
-		$description = str_replace("'", "", $description);
 		
-		$QUERY = "INSERT INTO cc_system_log (iduser, loglevel, action, description, tablename, pagename, ipaddress, data) ";
-		$QUERY .= " VALUES('".$userID."','".$logLevel."','".$actionPerformed."','".$description."','".$tableName."','".$pageName."','".$ipAddress."','".$data."')";
-		if ($this -> do_debug) echo $QUERY;
-
-		$table_log -> SQLExec($DB_Handle, $QUERY);
+		$res= $DB_Handle->Execute ("INSERT INTO cc_system_log (iduser, loglevel, action, description, tablename, pagename, ipaddress, data) ".
+				" VALUES(?,?,?,?,?,?,?,?);",
+				array($userID, $logLevel, $actionPerformed, $description, $tableName, $pageName, $ipAddress, $data));
+		
+		if (!$res) @syslog(LOG_WARNING,"Cannot log: ". $DB_Handle->ErrorMsg());
 	}
 	
 	//Funtion deleteLog
 	//Delete the log from table
 	function deleteLog($id = 0)
 	{
-		$DB_Handle = A2Billing::DBHandle();
-		$table_log = new Table();
-		$QUERY = "DELETE FROM cc_system_log WHERE id = ".$id;		
-		if ($this -> do_debug) echo $QUERY;		
-		$table_log -> SQLExec($DB_Handle, $QUERY);
+		throw new Exception("Why delete from syslog?");
+// 		$DB_Handle = A2Billing::DBHandle();
+// 		$QUERY = "DELETE FROM cc_system_log WHERE id = ".$id;
+// 		if ($this -> do_debug) echo $QUERY;		
+// 		$table_log -> SQLExec($DB_Handle, $QUERY);
 	}
 }
 
