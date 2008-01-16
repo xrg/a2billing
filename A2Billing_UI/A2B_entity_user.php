011c3db8958b30c9fbd4280db7872c2d6920744e
Panos Christeas <p_christeas@yahoo.com>
2008-01-17 01:04:18 +0200

    Preliminary ui_authen entity

--- A2Billing_UI/A2B_entity_user.php
+++ A2Billing_UI/A2B_entity_user.php
@@ -1,77 +1,33 @@
 <?php
+include ("./lib/defines.php");
+include ("./lib/module.access.php");
+include (DIR_COMMON."Form.inc.php");
+include (DIR_COMMON."Class.HelpElem.inc.php");
+include (DIR_COMMON."Form/Class.SqlRefField.inc.php");
+
 $menu_section='menu_admin';
-include ("../lib/defines.php");
-include ("../lib/module.access.php");
-include ("../lib/Form/Class.FormHandler.inc.php");
-include ("./form_data/FG_var_user.inc");
+
+HelpElem::DoHelp(_("Adminstrators"));
+
+$HD_Form= new FormHandler('cc_ui_authen',_("Administrators"),_("Administrator"));
+$HD_Form->checkRights(ACX_ADMINISTRATOR);
+$HD_Form->init();
+
+$PAGE_ELEMS[] = &$HD_Form;
+$PAGE_ELEMS[] = new AddNewButton($HD_Form);
+
+$HD_Form->model[] = new PKeyFieldEH(_("ID"),'userid','5%');
+$HD_Form->model[] = new TextFieldEH(_("Login"),'login');
+$HD_Form->model[] = dontList(new PasswdField(_("Password"),'password'));
+$HD_Form->model[] = new TextField(_("Name"),'name');
+$HD_Form->model[] = new IntField(_("Group"),'groupid');
+
+$HD_Form->model[] = new TextField(_("Direction"),'direction');
 
 
-if (! has_rights (ACX_ADMINISTRATOR)){ 
-	Header ("HTTP/1.0 401 Unauthorized");
-	Header ("Location: PP_error.php?c=accessdenied");	   
-	die();	   
-}
+// $HD_Form->model[] = new GroupField(array(new EditBtnField(),new DelBtnField()));
+$HD_Form->model[] = new DelBtnField();
 
-
-
-/***********************************************************************************/
-
-$HD_Form -> setDBHandler (DbConnect());
-
-// the include file
-$HD_Form -> init();
-
-$HD_Form -> FG_EDITION_LINK= $_SERVER['PHP_SELF']."?form_action=ask-edit&groupID=$groupID&userid=%#userid&";
-$HD_Form -> FG_DELETION_LINK= $_SERVER['PHP_SELF']."?form_action=ask-delete&groupID=$groupID&userid=%#userid&";
-
-
-if (!isset($form_action))  $form_action="list"; //ask-add
-if (!isset($action)) $action = $form_action;
-
-
-$list = $HD_Form -> perform_action($form_action);
-
-
-
-// #### HEADER SECTION
-include("PP_header.php");
-
-// #### HELP SECTION
-if($popup_select == "")
-{
-	if ($form_action == 'ask-add') echo $CC_help_admin_edit;
-	else echo $CC_help_admin_list;
-}
-if ($popup_select != ""){
-?>
-
-<SCRIPT LANGUAGE="javascript">
-<!-- Begin
-function sendValue(selvalue){
-	window.opener.document.<?php echo $popup_formname ?>.<?php echo $popup_fieldname ?>.value = selvalue;
-	window.close();
-}
-// End -->
-</script>
-
-<?php
-}
-
-
-// #### TOP SECTION PAGE
-$HD_Form -> create_toppage ($form_action);
-
-
-// #### CREATE FORM OR LIST
-//$HD_Form -> CV_TOPVIEWER = "menu";
-if (strlen($_GET["menu"])>0) $_SESSION["menu"] = $_GET["menu"];
-
-$HD_Form -> create_form ($form_action, $list, $id=null) ;
-
-// #### FOOTER SECTION
-include("PP_footer.php");
-
-
-
+require("PP_page.inc.php");
 
 ?>
