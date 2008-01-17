546eeb66e0d9ff132638813596052ed9f1de9bea
Panos Christeas <p_christeas@yahoo.com>
2008-01-18 00:06:08 +0200

    Call stats in menu

--- A2Billing_UI/PP_menu.inc.php
+++ A2Billing_UI/PP_menu.inc.php
@@ -204,6 +204,7 @@ function menu_show(sect_str){
 		<a onclick="menu_toggle('menu_creport');"><?= _("CALL REPORT");?></a>
 		<ul>
 			<li><a href="A2B_entity_call.php?"><?= _("Calls");?></a></li>
+			<li><a href="call-stats.php?"><?= _("Statistics");?></a></li>
 			<li><a href="call-log-customers.php?nodisplay=1&posted=1"><?= _("CDR Report");?></a></li>
 			<li><a href="call-comp.php?"><?= _("Calls Compare");?></a></li>
 			<li><a href="call-last-month.php?"><?= _("Monthly Traffic");?></a></li>
