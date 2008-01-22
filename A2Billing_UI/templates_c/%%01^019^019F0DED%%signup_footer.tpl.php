af99a581298de755b0e5aa02420796b345461553
areski <areski>
2008-01-22 17:00:10 +0000

    UPDATE : add tag 1.3.2

---
+++ A2Billing_UI/templates_c/%%01^019^019F0DED%%signup_footer.tpl.php
@@ -0,0 +1,9 @@
+<?php /* Smarty version 2.6.13, created on 2007-09-13 12:49:35
+         compiled from signup_footer.tpl */ ?>
+
+               <br></br>               
+               <br>
+              
+       </div>
+</body>
+</html>
---
+++ DataBase/mysql/Mysql-3.x_4.x/UPDATE-a2billing-v1.3.0-to-v1.3.1-mysql.sql
@@ -0,0 +1,2 @@
+
+ALTER TABLE cc_callback_spool CHANGE variable variable VARCHAR( 300 ) DEFAULT NULL;
