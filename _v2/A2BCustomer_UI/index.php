638fc9580ea84d50d96497ee1d2fd8cb81a0a88c
areski <areski>
2008-01-25 12:13:51 +0000

    UPDATE : update patch lib

---
+++ _v2/A2BCustomer_UI/index.php
@@ -0,0 +1,15 @@
+<?php
+include ("lib/customer.defines.php");
+include ("lib/customer.module.access.php");
+include ("lib/customer.smarty.php");
+
+
+$smarty->assign("error", $_GET["error"]);
+$password = base64_decode($_GET["password"]);
+
+$smarty->assign("username", $_GET["username"]);
+$smarty->assign("password", $password);
+
+$smarty->display('index.tpl');
+
+?>
