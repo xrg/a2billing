1d081fb7454811995e6040791fbd96a00e393b57
Panos Christeas <p_christeas@yahoo.com>
2008-03-01 12:30:43 +0200

    One todo, one test.

--- common/lib/Send_Mail.inc.php
+++ common/lib/Send_Mail.inc.php
@@ -27,6 +27,7 @@ function Send_Mails($dbg = 1,$dry = false){
 	if( $dbg>2)
 		echo "Mailer: start\n";
 	
+	// TODO: not only select, but lock mails in 'sending' state.
 	$qry = "SELECT cc_mailings.id AS id, mtype, fromname, fromemail, subject, message, defargs, tomail, args
 		FROM cc_templatemail, cc_mailings
 		WHERE cc_mailings.tmail_id = cc_templatemail.id
