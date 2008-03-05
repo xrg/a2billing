0650808a6dedd0265d826e94edc01afb4bbce6ff
Areski <areski@gmail.com>
2008-03-05 18:23:13 +0100

    Add few files : 28-vouchers.sql find-rate-optimization.txt ...

---
+++ DataBase/psql/build_database2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+A2B_USER=a2billing
+A2B_DB=a2billing_v200
+A2B_GROUP=a2b_group
+
+createdb -O $A2B_USER -U postgres $A2B_DB || exit $?
+
+psql -U postgres -d $A2B_DB --set ON_ERROR_STOP= -c 'CREATE LANGUAGE plpgsql;' || exit $?
+
+DO_TBLONLY=
+if [ "$1" == '--schema-only' ] ; then
+	DO_TBLONLY=y
+fi
+
+for DBSCRIPT in schema-200/* ; do
+	DBNUM=$(basename $DBSCRIPT | sed 's/^\([0-9]\+\)-.*$/\1/')
+	if [ "$DO_TBLONLY" == "y" ] && [ "$DBNUM" -ge 50 ] ; then
+		continue
+	fi
+	echo "Processing $DBSCRIPT.."
+	psql -U $A2B_USER --set ON_ERROR_STOP= --set A2B_GROUP=$A2B_GROUP \
+		-d $A2B_DB -f "$DBSCRIPT" || exit $?
+done
+
+echo "Database build successfully!"
+#eof
