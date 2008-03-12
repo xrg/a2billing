b54701402f7a0c92d2aff9313607ee5cebf8da95
Areski <areski@gmail.com>
2008-03-12 21:06:58 +0100

    add voucher support on AGI + stored procedure on DB for card using voucher

--- A2Billing_AGI/a2billing.php
+++ A2Billing_AGI/a2billing.php
@@ -124,7 +124,7 @@ $agi = new AGI($dynconf,'agiconf'.$idconfig);
 if (!$agi->is_alive)
 	exit();
 
-$mode = 'standard';
+// get the running mode -> DeadAGI(a2billing.php|1|voucher)
 if ($argc > 2 && strlen($argv[2]) > 0 )
 	switch ($argv[2]) {
 	case 'did':
@@ -136,7 +136,7 @@ if ($argc > 2 && strlen($argv[2]) > 0 )
 		$mode = $argv[2];
 		break;
 	default:
-		$agi->verbose("Unknown mode: ". $argv[2] );
+		$mode = 'standard';
 	}
 
 // get the area code for the cid-callback & all-callback
@@ -494,6 +494,98 @@ function getSpeedDial ($card, &$dialnum){
 	}
 }
 
+
+/**
+ *	Function refill_card_with_voucher
+ **/
+function getVoucher ($card){
+	global $a2b;
+	global $agi;
+	
+	$dbhandle =$a2b->DBHandle();
+	
+	$agi->conlog('Voucher refill with card',4);
+	
+	$voucher_prompt_enter = getAGIconfig('voucher-prompt-enter', 'prepaid-voucher_enter_number');
+	$voucher_timeout = getAGIconfig('voucher-timeoute', 8000);
+	$voucher_maxlen = getAGIconfig('voucher-maxlen', 15);
+	$voucher_minlen = getAGIconfig('voucher-minlen', 5);
+	$voucher_prompt = getAGIconfig('voucher-prompt', 'prepaid-voucher_enter_number');
+	$voucher_prompt_doesnt_exist = getAGIconfig('voucher-prompt-doesnt-exist', 'prepaid-voucher_does_not_exist');
+	$voucher_prompt_account_refill = getAGIconfig('voucher-prompt-account-refill', 'prepaid-account_refill');
+	$voucher_prompt_no_voucher_entered = getAGIconfig('voucher-prompt-no-voucher-entered', 'prepaid-no-voucher-entered');
+	$voucher_prompt_invalid_voucher = getAGIconfig('voucher-prompt-invalid-voucher', 'prepaid-invalid-voucher');
+	
+	$agi->conlog('Voucher-ivr: asking for Voucher',4);
+	$res_dtmf = $agi->get_data($voucher_prompt, $voucher_timeout, $voucher_maxlen);
+	
+	$agi->conlog('Voucher-ivr: result ' . print_r($res_dtmf,true),3);
+	if (!isset($res_dtmf['result'])){
+		$agi->conlog('No Voucher entered',2);
+		$agi-> stream_file($voucher_prompt_no_voucher_entered, '#');
+		return null;
+	}
+	$vouchernum = $res_dtmf['result'];
+	if ((strlen($vouchernum) < $voucher_minlen) || (strlen($vouchernum) > $voucher_maxlen)) {
+		$agi->conlog('Invalid Voucher',2);
+		$agi-> stream_file($voucher_prompt_invalid_voucher, '#');
+		return null;
+	}
+	
+	// CALL STORED PROCEDURE FOR VOUCHER
+	$QRY = str_dbparams($a2b->DBHandle(),'SELECT * FROM  card_use_voucher (%1, %2);',
+		array($card['id'],$vouchernum));
+		
+	$agi->conlog($QRY,3);
+	$res = $a2b->DBHandle()->Execute($QRY);
+	// If the rate engine has anything to Notice/Warn, display that..
+	if ($notice = $a2b->DBHandle()->NoticeMsg())
+		$agi->verbose('DB:' . $notice,2);
+	
+	if (!$res){
+		$emsg = $dbhandle->ErrorMsg();
+		if (substr($emsg,0,23) =='ERROR:  card_use_voucher'){
+			$msga= explode('|',$emsg);
+			$agi->verbose('Could not use voucher: '. $msga[3]);
+			//$agi->conlog("Message: " . print_r($msga,true),4);
+			switch ($msga[1]){
+			case 'voucher-no-find':
+				//$agi->stream_file('prepaid-card-in-use','#');
+				break;
+			case 'voucher-zero':
+			case 'conv_currency-failed':
+			case 'conv_currency-failed-zero':
+				//TODO
+				break;
+			default:
+				$agi->conlog('Unknown result from card_use_voucher: ' . $msga[1],3);
+			}
+		}else $agi->verbose('Could not use voucher : '. $emsg );
+		
+		$agi-> stream_file($voucher_prompt_invalid_voucher, '#');
+		return null;
+	}
+	if ($res->EOF){
+		$agi->verbose('No used voucher in card_use_voucher(), why?');
+		return null;
+	}
+	
+	$agi->conlog('Unknown result from card_use_voucher: ' . $msga[1],3);
+	
+	$row= $res->fetchRow();
+	if (empty($row['card_use_voucher'])){
+		$agi->verbose('Fail to fetch on voucher ! ');
+		return false;	
+	}
+	
+	$agi->conlog('Voucher used. Amount of credit added : ' . $row['card_use_voucher'], 3);
+	$agi-> stream_file($voucher_prompt_account_refill, '#');
+	
+	// TODO : play the Amount of credit added
+	
+	return true;
+}
+
 function formatDialstring($dialn,&$route, &$card){
 	global $agi;
 	$do_param = true;
@@ -687,7 +779,8 @@ function dialSpecial($dialnum,$route, $card,$last_prob,$agi){
 }
 
 
-if ($mode == 'standard'){
+
+if (($mode == 'standard') || ($mode == 'voucher')){
 
 	if (getAGIconfig('early_answer',false))
 		$agi->answer();
@@ -728,6 +821,11 @@ if ($mode == 'standard'){
 		}
 
 		// Here, we're authorized..
+		
+		if ($mode == 'voucher') {
+			getVoucher ($card);
+			break;
+		}
 		
 		/* We assume here that between consecutive attempts of calls, user's credit
 		   won't change! - we lock the card here */
@@ -955,8 +1053,8 @@ if ($mode == 'standard'){
 	$agi->hangup();
 	exit(0);
 
-	
-}// mode standard
+
+} // running mode
 
 //exit();
 ?>
