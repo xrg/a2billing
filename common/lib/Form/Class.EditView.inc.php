73f245be832129d120b226a42f3c5495b40f1c1f
Areski <areski@gmail.com>
2008-03-17 20:27:07 +0100

    New Tab feature for Edit View, let s check the concept :D

--- common/lib/Form/Class.EditView.inc.php
+++ common/lib/Form/Class.EditView.inc.php
@@ -105,6 +105,152 @@ class AskEditView extends FormView {
 }
 };
 
+
+
+class AskEditTabsView extends FormView {
+	protected $nb_fragment = 0;
+	 
+	public function Render(&$form){
+	// For convenience, ref the dbhandle locally
+	$dbhandle = &$form->a2billing->DBHandle();
+	
+	if ($form->FG_DEBUG>3)
+		echo "List! Building query..";
+		
+	
+	$query_fields = array();
+	$query_clauses = array();
+	foreach($form->model as $fld){
+		$tmp= $fld->editQueryField($dbhandle);
+		if ( is_string($tmp))
+			$query_fields[] = $tmp;
+		
+		$tmp= $fld->editQueryClause($dbhandle,$form);
+		if ( is_string($tmp))
+			$query_clauses[] = $tmp;
+	}
+	
+	if ($form->model_table == null){
+		if ($form->FG_DEBUG>0)
+			echo "No table!\n";
+		return;
+	}
+	
+	$QUERY = 'SELECT ';
+	if (count($query_fields)==0) {
+		if ($form->FG_DEBUG>0)
+			echo "No query fields!\n";
+		return;
+	}
+	
+	$QUERY .= implode(', ', $query_fields);
+	$QUERY .= ' FROM ' . $form->model_table;
+	
+	if (count($query_clauses))
+		$QUERY .= ' WHERE ' . implode(' AND ', $query_clauses);
+	
+	$QUERY .= ' LIMIT 1;'; // we can only edit one record at a time!
+	
+	if ($form->FG_DEBUG>3)
+		echo "QUERY: $QUERY\n<br>\n";
+	
+	// Perform the query
+	$res =$dbhandle->Execute($QUERY);
+	if (! $res){
+		if ($form->FG_DEBUG>0)
+			echo "Query Failed: ". nl2br(htmlspecialchars($dbhandle->ErrorMsg()));
+		return;
+	}
+	
+	if ($res->EOF) /*&& cur_page==0) */ {
+		if ($form->edit_no_records)
+			echo $edit_no_records;
+		else echo str_params(_("No %1 found!"),array($form->model_name_s),1);
+	} else {
+		// do the table..
+		$row=$res->fetchRow();
+		?>
+	<form action=<?= $_SERVER['PHP_SELF']?> method=post name="<?= $form->prefix?>Frm" id="<?= $form->prefix ?>Frm">
+	<?php
+		$hidden_arr = array('action' => 'edit','sub_action' => '');
+		foreach($form->model as $fld)
+			if ($arr2 = $fld->editHidden($row,$form))
+				$hidden_arr = array_merge($hidden_arr,$arr2);
+		if (strlen($form->prefix)>0){
+			$arr2= array();
+			foreach($hidden_arr as $key => $val)
+				$arr2[$form->prefix.$key] = $val;
+			$hidden_arr = $arr2;
+		}
+		$form->gen_PostParams($hidden_arr,true);
+		
+		foreach($form->model as $fld)
+			if ($fld && $fld->add_tab){
+				$this->nb_fragment++;
+				if ($this->nb_fragment==1) echo "\n<div id=\"rotate\"> <ul>\n";
+				echo '<li><a href="#fragment-'.$this->nb_fragment.'"><span>'.$fld->tablabel."</span></a></li>\n";
+			}
+		if ($this->nb_fragment > 0) echo "</ul>\n";
+
+	
+		$this->nb_fragment = 0;
+		$loopmodel = 0;
+		foreach($form->model as $fld){
+			if ($fld && $fld->add_tab){
+				$this->nb_fragment++;
+				if ($this->nb_fragment > 1){
+					?>
+					<tr class="confirm"><td colspan=2 align="right">
+					<button type=submit>
+					<?= str_params(_("Update this %1"),array($form->model_name_s),1) ?>
+					<img src="./Images/icon_arrow_orange.png" ></input>
+					<td>
+					</tr>
+					</tbody></table></div>
+					<?php
+				}
+				echo '<div id="fragment-'.$this->nb_fragment.'">';
+				if ($this->nb_fragment > 1){
+					?>
+					<table class="editForm" cellspacing="2">
+						<thead><tr><td class="field">&nbsp;</td><td class="value">&nbsp;</td></tr></thead>
+						<tbody>
+					<?php
+				}
+			}
+			if ($loopmodel == 0){
+				?>
+				<table class="editForm" cellspacing="2">
+					<thead><tr><td class="field">&nbsp;</td><td class="value">&nbsp;</td></tr></thead>
+					<tbody>
+				<?php
+			}
+			$loopmodel++;
+			
+			if ($fld && $fld->does_edit){
+		?><tr><td class="field"><?php
+				$fld->RenderEditTitle($form);
+		?></td><td class="value"><?php
+				$fld->DispEdit($row,$form);
+		?></td></tr>
+		<?php
+			}
+		}
+	?>
+	<tr class="confirm"><td colspan=2 align="right">
+	<button type=submit>
+	<?= str_params(_("Update this %1"),array($form->model_name_s),1) ?>
+	<img src="./Images/icon_arrow_orange.png" ></input>
+	<td>
+	</tr>
+	</tbody>
+	</table> </form>
+	<?php
+	if ($this->nb_fragment > 0) echo '</div></div>';
+	}
+}
+};
+
 class AskEdit2View extends AskEditView {
 };
 
