9b00ef37d9e130693817c844e240a24313297bf4
Panos Christeas <p_christeas@yahoo.com>
2008-03-20 16:47:29 +0200

    Ref. Agent in call list
    This was tricky, as a sub-query had to be used..

--- A2Billing_UI/call-stats.php
+++ A2Billing_UI/call-stats.php
@@ -18,13 +18,14 @@ $SEL_Form = new SelectionForm();
 $SEL_Form->init();
 $SEL_Form->model[] = new DateTimeField(_("Period from"),'date_from');
 	end($SEL_Form->model)->does_add = false;
-	end($SEL_Form->model)->def_date = '00:00 last month';
+	end($SEL_Form->model)->def_date = '00:00 - 10 days';
 	end($SEL_Form->model)->fieldexpr = 'starttime';
 $SEL_Form->model[] = new DateTimeField(_("Period to"),'date_to');
 	end($SEL_Form->model)->does_add = false;
 	end($SEL_Form->model)->def_date = 'now';
 	end($SEL_Form->model)->fieldexpr = 'starttime';
 
+$SEL_Form->fallbackClause=array('date_from');
 /*$SEL_Form->model[] = new SqlRefFieldN(_("Agent"),'agentid','cc_agent','id','name');
 	end($SEL_Form->model)->does_add = false;*/
 
@@ -35,6 +36,10 @@ $SEL_Form->model[] = new TextSearchField(_("Destination"),'destination');
 $SEL_Form->model[] = new TextSearchField(_("Called number"),'calledstation');
 /*$SEL_Form->model[] = new SqlRefField(_("Plan"),'idrp','cc_retailplan','id','name', _("Retail plan"));
 	end($SEL_Form->model)->does_add = false;*/
+$SEL_Form->model[] =dontAdd(new SqlRefFieldN(_("Agent"),'agid','cc_agent','id','name'));
+	end($SEL_Form->model)->fieldexpr='(SELECT agentid FROM cc_card,cc_card_group
+		WHERE cc_card.grp = cc_card_group.id AND cc_card.id = cc_call_v.cardid)';
+
 $SEL_Form->model[] =dontAdd(new SqlRefFieldN(_("Server"),'srvid','cc_a2b_server','id','host'));
 $SEL_Form->model[] = dontAdd(new SqlRefFieldN(_("Trunk"),'trunk','cc_trunk','id','trunkcode',
 		 _("Trunk used for the call")));
@@ -58,6 +63,9 @@ $sform->model[] = new DateField(_("Date"),'starttime');
 	end($sform->model)->fieldexpr='date_trunc(\'day\', starttime)';
 $sform->model[] = new TextField(_("Destination"), "destination");
 	end($sform->model)->fieldacr = _("Dest");
+$sform->model[] =new SqlRefFieldN(_("Agent"),'agid','cc_agent','id','login');
+	end($sform->model)->fieldexpr='(SELECT agentid FROM cc_card,cc_card_group
+		WHERE cc_card.grp = cc_card_group.id AND cc_card.id = cc_call_v.cardid)';
 
 $sform->model[] = new IntField(_("Calls"),'uniqueid');
 
@@ -84,6 +92,12 @@ $sform->views['sums']->sums[] = array('title' => _("Per day calls"),
 
 $sform->views['sums']->sums[] = array('title' => _("Per destination calls"),
 	'fns' => array( 'destination' =>true, 'uniqueid' => 'COUNT',
+		'sessiontime' => 'SUM', 'asr' => '', 'aloc' => 'AVG',
+		'sessionbill' => 'SUM', 'buycost' => 'SUM'),
+	'order' => 'COUNT(uniqueid)', 'sens' => 'DESC');
+
+$sform->views['sums']->sums[] = array('title' => _("Per agent calls"),
+	'fns' => array( 'agid' =>true, 'uniqueid' => 'COUNT',
 		'sessiontime' => 'SUM', 'asr' => '', 'aloc' => 'AVG',
 		'sessionbill' => 'SUM', 'buycost' => 'SUM'),
 	'order' => 'COUNT(uniqueid)', 'sens' => 'DESC');
