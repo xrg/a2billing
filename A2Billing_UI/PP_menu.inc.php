a2e7c981a7ac72e47bdd2f8340238415d1993f17
Panos Christeas <p_christeas@yahoo.com>
2008-03-31 12:39:26 +0300

    Menu.

--- A2Billing_UI/PP_menu.inc.php
+++ A2Billing_UI/PP_menu.inc.php
@@ -145,6 +145,7 @@ function menu_show(sect_str){
 			<li><a href="A2B_entity_buyrate.php?action=ask-import"><?= _("Import Buy Rates");?></a></li>
 			<li><a href="A2B_entity_sellrate.php?action=ask-import"><?= _("Import Sell Rates");?></a></li>
 			<li><a href="CC_entity_sim_ratecard.php"><?= _("Ratecard Simulator");?></a></li>
+			<li><a href="CC_entity_sim_ratecard.php"><?= _("DID Engine Simulator");?></a></li>
 		</ul>
 		</div>
 	<?php   }  ?>
