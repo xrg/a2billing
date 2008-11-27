37e7cc17c5790896770d8d963a953d85b15eaea2
Panos Christeas <p_christeas@yahoo.com>
2008-11-27 06:10:10 +0200

    SPA provision: different sip port per line.

--- common/lib/Provi/SpaXml_Provi.inc.php
+++ common/lib/Provi/SpaXml_Provi.inc.php
@@ -26,6 +26,7 @@ class SpaXmlProvi extends ProviEngine {
 		$qry = str_dbparams($dbhandle,'SELECT DISTINCT cc_card.*, cc_ast_users.devmodel, '.
 			' cc_ast_users.devsecret, cc_ast_users.callerid, cc_ast_users.defaultip, '.
 			' cc_ast_users.provi_name, cc_ast_users.provi_num, cc_ast_users.provi_date, '.
+			' (cc_ast_users.provi_num ::INTEGER + 5059) AS sip_num, ' .
 			' COALESCE(cc_ast_users.peernameb, cc_card.username) AS ext_name, '.
 			' COALESCE(cc_ast_users.secretb,cc_card.userpass) AS ext_password, ' .
 			' to_char(now(), \'HH24/MI\') AS localtime, to_char(now(), \'MM/DD\') AS localdate '.
