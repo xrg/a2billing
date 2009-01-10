6d123f48eff8fdb76a26c4192b4231a4615d7ae7
Panos Christeas <p_christeas@yahoo.com>
2009-01-10 12:36:44 +0200

    Parse sensord lines

--- addons/tests/parsers/logimport.php
+++ addons/tests/parsers/logimport.php
@@ -14,9 +14,6 @@ class SyslogImport extends ImportEngine {
 	
 	public function Init(array $args){
 		$this->args=$args;
-		if (!isset($args['db']))
-			throw new Exception("Please provide db in args!");
-		$this->dbh = $args['db'];
 	}
 
 	/** Reset the internal state of the parser */
@@ -65,8 +62,8 @@ class SyslogImport extends ImportEngine {
 					$this->last_date=$tokens[1];
 					$this->last_host=$tokens[2];
 					}
-				$this->reg_content($tokens[4]);
-					
+				if (!$this->reg_content($tokens[4]))
+					$this->out(LOG_WARNING,"Malformed line: $line2");
 			}elseif ($this->reg_special2($line2)==false)
 				$this->out(LOG_WARNING,"Unknown line: $line2");
 		}
@@ -87,6 +84,41 @@ class SensorsLogImport extends SyslogImport{
 	function SensorsLogImport(){
 		$this->fascility = "sensord";
 	}
+	public function Init(array $args){
+		parent::Init($args);
+		if (!isset($args['db']))
+			throw new Exception("Please provide db in args!");
+		$this->dbh = $args['db'];
+	}
+	
+	protected function find_host($host){
+		return $host;
+	}
+	protected function reg_msg(){
+		$this->out(LOG_DEBUG,"Found msg:". print_r($this->cur_par,True));
+	}
+	
+	protected function reg_par(){
+		$this->cur_par = array('date'=>$this->last_date, 
+			'sys' => $this->find_host($this->last_host),
+			'chip'=> null, 'adapter'=>null, 'sensors'=> array());
+	}
+	protected function reg_content($str){
+		//$this->cur_par .= $str."\n";
+		$toks=array();
+		if (preg_match('/^Chip: (\S+)$/',$str,$toks)>0)
+			$this->cur_par['chip']=$toks[1];
+		elseif (preg_match('/^Adapter: (.+)$/',$str,$toks)>0)
+			$this->cur_par['adapter']=$toks[1];
+		elseif (preg_match('/^ +-(.+)$/',$str,$toks)>0)
+			return True;
+		elseif (preg_match('/^ +(\S+): (\S+)$/',$str,$toks)>0)
+			$this->cur_par['sensors'][$toks[1]]=$toks[2];
+		else
+			return False;
+		return True;
+	}
+
 };
 
 ?>
